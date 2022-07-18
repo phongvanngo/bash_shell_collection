c7e32196ee95a1e1c6f8662de4605cfdfce558bd
phongvanngo <19520834@gm.uit.edu.vn>
2022-07-18 23:12:09 +0700

    store credential

--- git-clone-quan5-be.sh
+++ git-clone-quan5-be.sh
@@ -1,1 +1,2 @@
+git config credential.helper store
 git clone https://gitlab.com/webdevstudios.org/giao-viec-quan-5-backend
--- git-clone-quan5-fe.sh
+++ git-clone-quan5-fe.sh
@@ -1,1 +1,2 @@
+git config credential.helper store
 git clone https://gitlab.com/webdevstudios.org/website-giao-viec-quan-5
