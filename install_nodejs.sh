9a557d095405ad12cb483acc93d2a1c691eefb1b
phongvanngo <19520834@gm.uit.edu.vn>
2022-07-18 23:57:16 +0700

    update

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,3 +1,12 @@
+echo "install nodejs"
+
 apt-get install python-software-properties
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash –
 apt-get install nodejs
+
+echo "install npm"
+sudo apt install npm 
+
+echo "install yarn"
+npm i -g yarn
+
